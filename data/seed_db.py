a2d167aae099dad868a58e2e31d0c95116fdc7b8
psd314 <philippedixon@protonmail.com>
2019-05-29 03:44:42 -0400

    update seed_db script

--- data/seed_db.py
+++ data/seed_db.py
@@ -4,17 +4,17 @@ import sqlite3
 con = sqlite3.connect("imdb.db")
 c = con.cursor()
 
+c.execute('DROP TABLE IF EXISTS imdb')
+
 with open('movie_metadata.csv', newline='') as csv_file:
     reader = csv.reader(csv_file)
     is_header = True
     qs = ""
     for row in reader:
-	# get columns from header
         if is_header:
             header_cols = ', '.join(row)
-	    # prepare part of insert string based on length of columns in table
-            qs = ', '.join(['?']*28)
-            c.execute(f'CREATE TABLE IF NOT EXISTS imdb ({header_cols});')
+            qs = ', '.join(['?']*len(row))
+            c.execute(f'CREATE TABLE imdb ({header_cols});')
             is_header = False
         else:
             with con:
